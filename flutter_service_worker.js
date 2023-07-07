870c9368b255ffffbe1142cd0560030cdaaa3555
usamasabirr <75183736+usamasabirr@users.noreply.github.com>
2023-07-07 16:17:18 +0500

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4aa78f0c7b0a33a604db17d5f6893baa",
-"index.html": "c8a92b30e592287fa0044d3763722170",
-"/": "c8a92b30e592287fa0044d3763722170",
-"main.dart.js": "95a07a94f0deadea6b572567ba260167",
+"index.html": "911d29e9499ab648d32bf85afd70f633",
+"/": "911d29e9499ab648d32bf85afd70f633",
+"main.dart.js": "cff2e9d4f9d7ceed476a97de21d84fa6",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "cec06261144dd5f33864440510a04c82",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
